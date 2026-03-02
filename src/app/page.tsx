07697bb030a54179572d41f8d367da8bb5d9b277
jeangoi483-droid <jeangoi483@gmail.com>
2026-03-02 17:37:33 +0000

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import Navbar from "@/components/Navbar";
+
+export default function Home() {
+  return (
+    <div>
+      <Navbar />
+      <h2 style={{ padding: "20px" }}>
+        Bienvenue sur CEB Shop 🚀
+      </h2>
+    </div>
+  );
+}
