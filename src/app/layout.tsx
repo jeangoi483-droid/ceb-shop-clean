2154dda44da5894a56bf04a4b650964e4c0dec94
jeangoi483-droid <jeangoi483@gmail.com>
2026-03-02 17:36:52 +0000

    Create layout.tsx

---
+++ src/app/layout.tsx
@@ -0,0 +1,16 @@
+export const metadata = {
+  title: "CEB Shop",
+  description: "Ma boutique en ligne",
+};
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="fr">
+      <body>{children}</body>
+    </html>
+  );
+}
